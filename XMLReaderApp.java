d51b83cdcdcf305eb2be7e00569a5ca8e8287282
Tumi <g22m3544@campus.ru.ac.za>
2025-04-16 19:24:44 +0200

    Added a filter to allow users to select the field they want to display

--- XMLReaderApp.java
+++ XMLReaderApp.java
@@ -1,51 +1,67 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
-import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.*;
+
+import java.io.File;
+import java.util.*;
 
 public class XMLReaderApp {
-   public XMLReaderApp() {
-   }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-   public static void main(String[] var0) {
-      try {
-         File var1 = new File("C:\\Users\\User\\Desktop\\TaskToo\\tasktoo\\data.xml");
-         DocumentBuilderFactory var2 = DocumentBuilderFactory.newInstance();
-         var2.setNamespaceAware(true);
-         DocumentBuilder var3 = var2.newDocumentBuilder();
-         Document var4 = var3.parse(var1);
-         var4.getDocumentElement().normalize();
-         NodeList var5 = var4.getElementsByTagName("record");
+        // Ask user for fields to print
+        System.out.println("Enter the fields you want to print (comma-separated), e.g. name,region,country:");
+        String input = scanner.nextLine();
+        String[] requestedFields = input.split(",");
 
-         for(int var6 = 0; var6 < var5.getLength(); ++var6) {
-            Node var7 = var5.item(var6);
-            if (var7.getNodeType() == 1) {
-               Element var8 = (Element)var7;
-               String var9 = getTagValue("name", var8);
-               String var10 = getTagValue("list", var8);
-               System.out.println("Name: " + var9);
-               System.out.println("Value: " + var10);
-               System.out.println("--------------");
+        // Clean up field names (remove spaces)
+        Set<String> fieldSet = new HashSet<>();
+        for (String field : requestedFields) {
+            fieldSet.add(field.trim().toLowerCase());
+        }
+
+        try {
+            // Update this path if needed
+            File file = new File("C:\\Users\\User\\Desktop\\TaskToo\\tasktoo\\data.xml");
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(file);
+            doc.getDocumentElement().normalize();
+
+            NodeList records = doc.getElementsByTagName("record");
+
+            for (int i = 0; i < records.getLength(); i++) {
+                Node node = records.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+
+                    System.out.println("Record " + (i + 1));
+                    for (String field : fieldSet) {
+                        String value = getTagValue(field, element);
+                        if (!value.isEmpty()) {
+                            System.out.println(field + ": " + value);
+                        }
+                    }
+                    System.out.println("-------------------------");
+                }
             }
-         }
-      } catch (Exception var11) {
-         var11.printStackTrace();
-      }
 
-   }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
 
-   private static String getTagValue(String var0, Element var1) {
-      NodeList var2 = var1.getElementsByTagName(var0);
-      if (var2.getLength() == 0) {
-         return "";
-      } else {
-         NodeList var3 = var2.item(0).getChildNodes();
-         Node var4 = var3.item(0);
-         return var4 != null ? var4.getNodeValue() : "";
-      }
-   }
+        scanner.close();
+    }
+
+    private static String getTagValue(String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag);
+        if (nodeList.getLength() == 0) return "";
+        NodeList children = nodeList.item(0).getChildNodes();
+        Node node = (children != null && children.getLength() > 0) ? children.item(0) : null;
+        return node != null ? node.getNodeValue() : "";
+    }
 }
