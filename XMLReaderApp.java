0993fe342ec086856084b9a710b4459b346d270f
Tumi <g22m3544@campus.ru.ac.za>
2025-04-16 19:08:43 +0200

    Corrected path in line 16

--- XMLReaderApp.java
+++ XMLReaderApp.java
@@ -1,52 +1,51 @@
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.*;
-
+// Source code is decompiled from a .class file using FernFlower decompiler.
 import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class XMLReaderApp {
-    public static void main(String[] args) {
-        try {
-            // Replace with the path to your saved file
-            File file = new File("data.xml"); // or "data.html" if it's HTML with embedded XML
+   public XMLReaderApp() {
+   }
 
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            factory.setNamespaceAware(true);
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(file);
+   public static void main(String[] var0) {
+      try {
+         File var1 = new File("C:\\Users\\User\\Desktop\\TaskToo\\tasktoo.html");
+         DocumentBuilderFactory var2 = DocumentBuilderFactory.newInstance();
+         var2.setNamespaceAware(true);
+         DocumentBuilder var3 = var2.newDocumentBuilder();
+         Document var4 = var3.parse(var1);
+         var4.getDocumentElement().normalize();
+         NodeList var5 = var4.getElementsByTagName("record");
 
-            doc.getDocumentElement().normalize();
-
-            // Replace "record" with the relevant XML parent tag you're targeting
-            NodeList nodes = doc.getElementsByTagName("record");
-
-            for (int i = 0; i < nodes.getLength(); i++) {
-                Node node = nodes.item(i);
-
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-
-                    // Example field names
-                    String name = getTagValue("name", element);
-                    String value = getTagValue("", element);
-
-                    System.out.println("Name: " + name);
-                    System.out.println("Value: " + value);
-                    System.out.println("--------------");
-                }
+         for(int var6 = 0; var6 < var5.getLength(); ++var6) {
+            Node var7 = var5.item(var6);
+            if (var7.getNodeType() == 1) {
+               Element var8 = (Element)var7;
+               String var9 = getTagValue("name", var8);
+               String var10 = getTagValue("list", var8);
+               System.out.println("Name: " + var9);
+               System.out.println("Value: " + var10);
+               System.out.println("--------------");
             }
+         }
+      } catch (Exception var11) {
+         var11.printStackTrace();
+      }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+   }
 
-    // Helper function to safely extract field values
-    private static String getTagValue(String tag, Element element) {
-        NodeList tagList = element.getElementsByTagName(tag);
-        if (tagList.getLength() == 0) return "";
-        NodeList list = tagList.item(0).getChildNodes();
-        Node node = (Node) list.item(0);
-        return node != null ? node.getNodeValue() : "";
-    }
+   private static String getTagValue(String var0, Element var1) {
+      NodeList var2 = var1.getElementsByTagName(var0);
+      if (var2.getLength() == 0) {
+         return "";
+      } else {
+         NodeList var3 = var2.item(0).getChildNodes();
+         Node var4 = var3.item(0);
+         return var4 != null ? var4.getNodeValue() : "";
+      }
+   }
 }
